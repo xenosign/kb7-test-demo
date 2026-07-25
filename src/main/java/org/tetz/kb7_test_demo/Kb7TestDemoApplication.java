8ae4a469ace4030ef79160baf193a76295f3db91
kdtTetz <xenosign@naver.com>
2026-07-25 18:39:01 +0900

    Initial commit

---
+++ src/main/java/org/tetz/kb7_test_demo/Kb7TestDemoApplication.java
@@ -0,0 +1,13 @@
+package org.tetz.kb7_test_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kb7TestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kb7TestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tetz/kb7_test_demo/Kb7TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tetz.kb7_test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kb7TestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
